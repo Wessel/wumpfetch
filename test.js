6fd012bfe8d34cd3284dfd1c3b65992c76884888
Wessel T <discord@go2it.eu>
2019-02-16 15:14:29 +0100

    Use lumah for testing

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
+const u = require('util');
+const l = require('lumah');
 const w = require('./createRequest.js');
 
-;( async() => {
+l.register('Fetch a cat image', async (end) => {
 	const r = await w({ url: 'https://aws.random.cat/meow', parse: 'json' }).send();
-	console.log(r.body);
-})();
+	end(r.statusCode === 200 ? true : false, r.statusCode === 200 ? u.inspect(r.body) : r.statusCode);
+});
+
+l.start();
