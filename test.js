584c7afb34b9fb476180280d99df39bad3ab8c5b
David Ralph <ohlookitsderpy@protonmail.com>
2019-02-18 12:11:30 +0000

    yes

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-const u = require('util');
-const l = require('lumah');
-const w = require('./createRequest.js');
+const w                   = require('./createRequest.js');
+const { inspect }         = require('util');
+const { register, start } = require('lumah');
 
-l.register('Fetch a cat image', async (end) => {
+register('Fetch a cat image', async (end) => {
 	const r = await w({ url: 'https://aws.random.cat/meow', parse: 'json' }).send();
-	end(r.statusCode === 200 ? true : false, r.statusCode === 200 ? u.inspect(r.body) : r.statusCode);
+	end(r.statusCode === 200 ? true : false, r.statusCode === 200 ? inspect(r.body) : r.statusCode);
 });
 
-l.start();
+start();
