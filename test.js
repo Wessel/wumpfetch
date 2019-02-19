ed6cd09c9234559b2190a86c3765099fda7408b9
Wessel T <discord@go2it.eu>
2019-02-19 19:32:31 +0100

    Add minified versions

--- test.js
+++ test.js
@@ -2,8 +2,6 @@ const u = require('util');
 const l = require('lumah');
 const w = require('./createRequest.js');
 
-console.log(w)
-
 l.register('Fetch cat image A', async (end) => {
 	const r = await w({ url: 'https://aws.random.cat/meow', parse: 'json' }).send();
 	end(r.statusCode === 200 ? true : false, r.statusCode === 200 ? u.inspect(r.body) : r.statusCode);
